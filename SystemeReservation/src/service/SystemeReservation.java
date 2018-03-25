32922013d44f3cf56966d45aa6040d1c31759594
H <H@DESKTOP-7OENLF5>
2018-03-25 00:54:01 -0400

    progress systemeReservation admin

--- SystemeReservation/src/service/SystemeReservation.java
+++ SystemeReservation/src/service/SystemeReservation.java
@@ -1,5 +1,39 @@
 package service;
 
+import gestion.OperationsAdmin;
+import repository.AvionModel;
+import repository.CroisiereModel;
+import repository.Model;
+import repository.TrainModel;
+
 public class SystemeReservation {
 
+	private Model avionModel;
+	private Model croisiereModel;
+	private Model trainModel;
+	
+	private OperationsAdmin adminVols;
+	private OperationsAdmin adminCroisieres;
+	private OperationsAdmin adminTrains;
+	
+	public SystemeReservation() {
+		avionModel = new AvionModel();
+		croisiereModel = new CroisiereModel();
+		trainModel = new TrainModel();
+		
+		adminVols = new OperationsAdmin(avionModel);
+		adminCroisieres = new OperationsAdmin(croisiereModel);
+		adminTrains = new OperationsAdmin(trainModel);
+		
+		
+		
+		
+		
+		avionModel.attach(adminVols);
+		croisiereModel.attach(adminCroisieres);
+		trainModel.attach(adminTrains);
+	}
+	
+	
+	
 }
